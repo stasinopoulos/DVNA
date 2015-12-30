4f6a226bc79ebab22d12b02fa67b79ed70c4176f
Anastasios Stasinopoulos <an.stasinopoulos@gmail.com>
2015-12-30 13:38:00 +0200

    Update command_injection.js

--- vulnerabilities/command_injection.js
+++ vulnerabilities/command_injection.js
@@ -4,7 +4,7 @@ var exe = require("child_process");
 http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true);
   response.writeHead(200, {"Content-Type" : "text/html"});
-  exe.exec('id', function(err, data) {
+  exe.exec('ping -c2 '+ parsedUrl.query.ping, function(err, data) {
     response.write(":~/$ " + data);
     console.log(data);
     response.end();
